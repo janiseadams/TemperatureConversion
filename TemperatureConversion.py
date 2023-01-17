a93d65cb9bf8f6f28b3c0a2c9a70dae377dccf86
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-16 17:05:30 -0800

    Janise Adams CIS261 TemperatureConversion

--- TemperatureConversion.py
+++ TemperatureConversion.py
@@ -1,1 +1,14 @@
+def tocels(fahrenheit):
+    celsius = ((fahrenheit -32)*5)/9
+    return celsius
 
+def tofah(celsius):
+    fahn = ((celsius*9)/5) + 32
+    return fahn
+
+if __name__ == "__main__":
+    for temps in range(0, 212, 40):
+        print(temps, "Fahrenheit = ", round(tocels(temps), 2), "Celsius")
+
+    for temps in range(0, 100, 20):
+        print(temps, "Celsius = ", round(tofah(temps) , 2), "Fahrenheit")
